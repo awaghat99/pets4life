96331f4a95ebc795ef68793f40fb568b5bbb4e2d
awaghat99 <awaghat99@gmail.com>
2023-09-06 13:03:40 +0100

    fetch api displaying images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,18 @@ import { useState, useEffect } from "react";
 const Home = () => {
   const [catData, setCatData] = useState();
 
-  const apiKey = "live_a49d14G4QIFGKTDT4vMm7ZlGDTCMhjTTcluXKodeyxclY7XubXPGvHa55wY6eNIf";
-  const url = `https://api.thecatapi.com/v1/images/search?limit=100?api_key=${apiKey}`;
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(
+          `https://api.thecatapi.com/v1/images/search?limit=100&api_key=live_a49d14G4QIFGKTDT4vMm7ZlGDTCMhjTTcluXKodeyxclY7XubXPGvHa55wY6eNIf`
+        );
         if (!response.ok) {
           throw new Error(response.statusText);
         }
         const data = await response.json();
         console.log(data);
+        setCatData(data);
       } catch (e) {
         console.log(e.message);
       }
@@ -23,7 +23,17 @@ const Home = () => {
     fetchData();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      {[
+        catData
+          ? catData.map((cat, index) => {
+              return <img key={index} src={cat.url} alt={`cat-${index}`} />;
+            })
+          : "Loading Cat Data",
+      ]}
+    </div>
+  );
 };
 
 export default Home;
