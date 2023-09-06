36d973f735cfbd8f38b98bb0f1422fdece620562
awaghat99 <awaghat99@gmail.com>
2023-09-06 13:38:15 +0100

    created grid for catCards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import CatItem from "../components/CatCard";
+import "../App.css";
 
 const Home = () => {
   const [catData, setCatData] = useState();
+  const createRandomPrice = () => {
+    return Math.floor(Math.random() * 250);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `https://api.thecatapi.com/v1/images/search?limit=100&api_key=live_a49d14G4QIFGKTDT4vMm7ZlGDTCMhjTTcluXKodeyxclY7XubXPGvHa55wY6eNIf`
+          `https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=99&api_key=live_a49d14G4QIFGKTDT4vMm7ZlGDTCMhjTTcluXKodeyxclY7XubXPGvHa55wY6eNIf`
         );
         if (!response.ok) {
           throw new Error(response.statusText);
@@ -25,13 +30,15 @@ const Home = () => {
 
   return (
     <div>
-      {[
-        catData
-          ? catData.map((cat, index) => {
-              return <img key={index} src={cat.url} alt={`cat-${index}`} />;
-            })
-          : "Loading Cat Data",
-      ]}
+      <div className="all-cats">
+        {[
+          catData
+            ? catData.map((cat, index) => {
+                return <CatItem key={index} cat={cat} price={createRandomPrice()} />;
+              })
+            : "Loading Cat Data",
+        ]}
+      </div>
     </div>
   );
 };
