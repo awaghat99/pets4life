b840d2f25993e5fe2aa3f7aafb60e383c6384064
awaghat99 <awaghat99@gmail.com>
2023-09-07 13:13:43 +0100

    fix displayedCats error

--- src/components/BasketItem.js
+++ src/components/BasketItem.js
@@ -6,9 +6,8 @@ const BasketItem = () => {
       <div className="Basket-image">
         <img src="https://cdn2.thecatapi.com/images/ef1.jpg" alt="cat" />
       </div>
-      <div className="Basket-price">
+      <div className="Basket-price-and-breed">
         <h3>£100</h3>
-
         <h2>British Short-hair</h2>
       </div>
     </div>
