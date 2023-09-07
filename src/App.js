b05e8427b225475590a63affdd2ec069722dd7c2
awaghat99 <awaghat99@gmail.com>
2023-09-07 10:33:57 +0100

    move fetch to app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/NavBar";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [catData, setCatData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=99&api_key=live_a49d14G4QIFGKTDT4vMm7ZlGDTCMhjTTcluXKodeyxclY7XubXPGvHa55wY6eNIf`
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        setCatData(data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/Home" element={<Home catData={catData} />} />
           <Route path="/About" element={<About />} />
         </Routes>
       </div>
